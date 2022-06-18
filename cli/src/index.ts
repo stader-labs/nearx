a73f4c27fc8451c702bc21d570deb3404d609800
Félix <felix@staderlabs.com>
2022-06-18 08:01:38 +0200

    CLI skeletton

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,1 +1,38 @@
-console.log('hello world');
+const commands: { [name: string]: () => Promise<void> } = {
+  stake: stake,
+  unstake: unstake,
+  withdraw: withdraw,
+};
+
+async function run(name: string) {
+  if (name in commands) {
+    await commands[name]();
+  } else {
+    if (name != null) {
+      console.error('Undefined command:', name);
+    }
+    error();
+  }
+}
+
+function error(message?: string): never {
+  console.error(message ?? help);
+  process.exit(1);
+}
+
+const help: string = `./nearx COMMAND
+    COMMAND: ${Object.keys(commands).join(' | ')}`;
+
+async function stake() {
+  console.log('stake');
+}
+
+async function unstake() {
+  console.log('unstake');
+}
+
+async function withdraw() {
+  console.log('withdraw');
+}
+
+run(process.argv[2]).then(() => console.log('Command successfully executed'));
