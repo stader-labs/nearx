b2b373e506bbd7f4fd38b4b0fd0c025226f027e1
Bharath Vedartham <vedabharath12345@gmail.com>
2022-06-22 15:28:35 +0530

    Add contract interaction scripts

---
+++ scripts/state_view.sh
@@ -0,0 +1,9 @@
+near view $CONTRACT_NAME get_nearx_pool_state
+
+near view $CONTRACT_NAME get_nearx_price
+
+near view $CONTRACT_NAME get_total_staked
+
+near view $CONTRACT_NAME get_validators
+
+near view $CONTRACT_NAME get_validator_info '{"validator": "'"$STAKE_POOL_0"'"}'
---
+++ scripts/user_actions.sh
@@ -0,0 +1,13 @@
+
+# deposits
+for i in {1..10};
+  do near call $CONTRACT_NAME deposit_and_stake --accountId=$ID --amount=1 --gas=300000000000000;
+done;
+
+# unstaking
+near call $CONTRACT_NAME unstake '{"amount": "3000000000000000000000000"}' --accountId=$ID --gas=300000000000000;
+near call $CONTRACT_NAME unstake_all --accountId=$ID --gas=300000000000000;
+
+# withdraw
+near call $CONTRACT_NAME withdraw '{"amount": "3000000000000000000000000"}' --accountId=$ID --gas=300000000000000;
+near call $CONTRACT_NAME withdraw_all --accountId=$ID --gas=300000000000000;
