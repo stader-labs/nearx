a987449bf47fe824b8fefea1f649e93aa2730216
Bharath Vedartham <vedabharath12345@gmail.com>
2022-08-07 00:27:38 +0530

    Write some scripts

--- scripts/dev-testing.sh
+++ scripts/dev-testing.sh
@@ -7,13 +7,18 @@ near call $CONTRACT_NAME add_validator '{"validator": "'"$STAKE_POOL_2"'", "weig
 near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_0"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
 near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_1"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
 near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_2"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
+near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_0"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
+near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_1"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
+near call $CONTRACT_NAME manager_deposit_and_stake '{"validator": "'"$STAKE_POOL_2"'"}'  --accountId=$ID --amount=1 --gas=300000000000000;
 
 # 10 deposits
 for i in {1..3};
 do near call $CONTRACT_NAME deposit_and_stake --accountId=$ID --amount=1 --gas=300000000000000;
 done;
 
-near call $CONTRACT_NAME unstake '{"amount": "1000000000000000000000000"}' --accountId=$ID --gas=300000000000000;
+near call $CONTRACT_NAME unstake '{"amount": "4000000000000000000000000"}' --accountId=$ID --gas=300000000000000;
+
+near call $CONTRACT_NAME storage_deposit --accountId=$ID --gas=300000000000000 --depositYocto=3000000000000000000000;
 
 near view $CONTRACT_NAME ft_balance_of '{"account_id": "'"$ID"'"}'
 near view $CONTRACT_NAME ft_balance_of '{"account_id": "elder_joy.near"}'
