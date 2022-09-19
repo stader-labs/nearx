05dc71717146b80e15e0b1e388fe98b159a48488
Bharath Vedartham <vedabharath12345@gmail.com>
2022-09-19 16:53:48 +0530

    Update upgrade.ts

--- client/src/upgrade.ts
+++ client/src/upgrade.ts
@@ -7,13 +7,5 @@ const upgrade = async (
   accountId: string,
 ) => {
   const nearxPoolClient = await NearxPoolClient.new(environment, contract, accountId);
-  // nearxPoolClient.near.
   await nearxPoolClient.contractUpgrade(fileName);
 };
-
-// upgrade(
-//   '/Users/bharath12345/stader-work/near-liquid-token/res/near_x.wasm',
-//   'mainnet',
-//   'v2-nearx.stader-labs.near',
-//   'stader-labs.near',
-// ).then(console.log);
